3eb4c4588a7e7b692d2980c9b2d6d45df47d3b14
Andrew Carletti <andrew.carletti25@gmail.com>
2025-07-22 16:32:00 -0300

    feat: create layout structure

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,16 @@
 import { Route, Routes } from "react-router-dom";
+
+import DefaultLayout from "./layouts/DefaultLayout";
 import HistoryPage from "./pages/HistoryPage";
 import HomePage from "./pages/HomePage";
 
 export default function App() {
   return (
     <Routes>
-      <Route path="/" element={<HomePage />} />
-      <Route path="/history" element={<HistoryPage />} />
+      <Route path="/" element={<DefaultLayout />}>
+        <Route path="/" element={<HomePage />} />
+        <Route path="/history" element={<HistoryPage />} />
+      </Route>
     </Routes>
   );
 }
